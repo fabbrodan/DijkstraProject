24f8a7eb3a22641da3718be34afe74c1dcb27108
Daniel Åslund <daniel.aaslund@gmail.com>
2020-04-30 11:35:58 +0200

    Added additional logic to nodemap generation and Z-Index to drawing

--- DijkstraApp/Form1.Designer.cs
+++ DijkstraApp/Form1.Designer.cs
@@ -29,31 +29,69 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.generateNodeMap_btn = new System.Windows.Forms.Button();
+            this.nodeCount_tb = new System.Windows.Forms.TextBox();
+            this.nodeNumberLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(776, 355);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
+            // generateNodeMap_btn
+            // 
+            this.generateNodeMap_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.generateNodeMap_btn.Location = new System.Drawing.Point(12, 486);
+            this.generateNodeMap_btn.Name = "generateNodeMap_btn";
+            this.generateNodeMap_btn.Size = new System.Drawing.Size(133, 37);
+            this.generateNodeMap_btn.TabIndex = 2;
+            this.generateNodeMap_btn.Text = "Generate";
+            this.generateNodeMap_btn.UseVisualStyleBackColor = true;
+            this.generateNodeMap_btn.Click += new System.EventHandler(this.generateNodeMap_btn_Click);
+            // 
+            // nodeCount_tb
+            // 
+            this.nodeCount_tb.Location = new System.Drawing.Point(12, 460);
+            this.nodeCount_tb.Name = "nodeCount_tb";
+            this.nodeCount_tb.Size = new System.Drawing.Size(132, 20);
+            this.nodeCount_tb.TabIndex = 1;
+            // 
+            // nodeNumberLabel
+            // 
+            this.nodeNumberLabel.AutoSize = true;
+            this.nodeNumberLabel.Location = new System.Drawing.Point(12, 441);
+            this.nodeNumberLabel.Name = "nodeNumberLabel";
+            this.nodeNumberLabel.Size = new System.Drawing.Size(73, 13);
+            this.nodeNumberLabel.TabIndex = 4;
+            this.nodeNumberLabel.Text = "No. of Nodes:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 535);
+            this.Controls.Add(this.nodeNumberLabel);
+            this.Controls.Add(this.nodeCount_tb);
+            this.Controls.Add(this.generateNodeMap_btn);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button generateNodeMap_btn;
+        private System.Windows.Forms.TextBox nodeCount_tb;
+        private System.Windows.Forms.Label nodeNumberLabel;
     }
 }
 
--- DijkstraApp/Form1.cs
+++ DijkstraApp/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using DijkstraLib;
 
@@ -14,25 +9,67 @@ namespace DijkstraApp
     public partial class Form1 : Form
     {
         private NodeMap NodeMap = new NodeMap();
+        private Color[] colors = { Color.Red, Color.Blue, Color.Green, Color.Cyan, Color.Yellow, Color.BlueViolet };
         public Form1()
         {
             InitializeComponent();
-            NodeMap.GenerateRandomNodes(10, 10, panel1.Height, panel1.Width);
+            nodeCount_tb.Text = "3";
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            foreach (Node node in NodeMap.Nodes)
+            if (NodeMap.Nodes.Count > 0)
             {
-                g.DrawEllipse(new Pen(Color.Blue), new RectangleF(node.Position.X, node.Position.Y, 20, 20));
-                g.DrawString(node.NodeName, new Font("Arial", 12), new SolidBrush(Color.Red), node.Position.X, node.Position.Y);
-                foreach (KeyValuePair<Node, int> connectedNode in node.ConnectedNodes)
+                Graphics g = e.Graphics;
+                int ellipseSize = 30;
+                int lineOffset = (int)(ellipseSize /2);
+                int textOffset = (int)(ellipseSize * 0.1);
+                foreach (Node node in NodeMap.Nodes)
                 {
-                    g.DrawLine(new Pen(Color.Black, 1), node.Position.X + 10, node.Position.Y + 10, connectedNode.Key.Position.X + 10, connectedNode.Key.Position.Y + 10);
-                    //g.DrawString(connectedNode.Value.ToString(), new Font("Arial", 12), new SolidBrush(Color.Red), new PointF((node.Position.X + connectedNode.Key.Position.X) / 2, (node.Position.Y + connectedNode.Key.Position.Y) / 2));
+                    int colorRandom = 0;
+                    foreach (KeyValuePair<Node, int> connectedNode in node.ConnectedNodes)
+                    {
+                        g.DrawLine(new Pen(colors[colorRandom], 2),
+                            node.Position.X + lineOffset,
+                            node.Position.Y + lineOffset,
+                            connectedNode.Key.Position.X + lineOffset,
+                            connectedNode.Key.Position.Y + lineOffset);
+                        if (colorRandom == 5)
+                        {
+                            colorRandom = 0;
+                        }
+                        else
+                        {
+                            colorRandom++;
+                        }
+                    }
+                }
+
+                foreach (Node node in NodeMap.Nodes)
+                {
+                    g.FillEllipse(new SolidBrush(Color.Black), new RectangleF(node.Position.X, node.Position.Y, ellipseSize, ellipseSize));
+                    g.DrawString(node.NodeName, new Font("Arial", 12, FontStyle.Bold), new SolidBrush(Color.Red), node.Position.X + textOffset, node.Position.Y + textOffset);
                 }
             }
+        }
+
+        private void generateNodeMap_btn_Click(object sender, EventArgs e)
+        {
+            int noOfNodes = Int32.Parse(nodeCount_tb.Text);
+            if (noOfNodes < 3)
+            {
+                MessageBox.Show("You need to enter at least three!");
+                return;
+            }
+            if (noOfNodes > 26)
+            {
+                MessageBox.Show("You cannot enter more than 26!");
+                return;
+            }
+            NodeMap.Nodes.Clear();
+            NodeMap.GenerateRandomNodes(noOfNodes, 10, panel1.Height, panel1.Width);
+
+            panel1.Invalidate();
         }
     }
 }
